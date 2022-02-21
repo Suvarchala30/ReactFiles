89552d557a4721816299e2a7111ec8bf8ac77d1b
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-21 23:03:41 +0530

    feb 21

--- first-app/src/components/Component1.js
+++ first-app/src/components/Component1.js
@@ -1,10 +1,11 @@
 import React from "react";//This is optional in function based components
-
+import Developer from "./Developer";
 function Component1(){
     return (
         <div className="Component1">
         <h1>A Heading</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, alias ea. Exercitationem sint veniam sunt laborum eligendi earum neque vitae maxime. Accusantium earum cum ipsam porro magni rem iste odit.</p>
+        <Developer />
         </div>
     )
 }
---
+++ first-app/src/components/Developer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Developer(){
+    return (
+        <div className="Developer">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoSj-CCKyif7BMEOklKuvbTLyfC8OUYMuXFw&usqp=CAU" />
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem doloribus reprehenderit explicabo sed mollitia quidem dolores, sunt maxime pariatur quis laboriosam repellat, asperiores iste. Tenetur quo natus tempore fuga dicta, eaque ullam velit autem nostrum perspiciatis neque. Ratione atque dolor minima ipsa temporibus, voluptates ea iste magni amet inventore necessitatibus nulla odit! Beatae quis nisi esse ex, ipsa consequuntur necessitatibus unde vero aliquid omnis nulla aspernatur enim blanditiis rerum consectetur dolorum eos error non asperiores optio, cupiditate ea libero? Magni eos fuga repellendus magnam cumque numquam dolor exercitationem beatae aspernatur, quae inventore quo doloribus quam tenetur. Unde alias assumenda quisquam!</p>
+        </div>
+    )
+}
+
+export default Developer
