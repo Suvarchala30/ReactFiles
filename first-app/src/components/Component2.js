d078dd1a15e8f3eb5a36d31169dc02cd4c60ec92
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-21 22:28:30 +0530

    components

--- first-app/src/components/Component2.js
+++ first-app/src/components/Component2.js
@@ -1,22 +1,13 @@
-//import logo from './logo.svg';
-//import './App.css';
-import Component1 from "./components/Component1";
-import Component2 from "./components/Component2";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    <Component1 />
-    <h1>Heading 1</h1>
+function Component2(){
+    return (
+    <div className="Component2">
+        <h1>Heading 3</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium consequuntur nihil voluptatibus, nostrum perferendis hic cum eius fuga, animi reprehenderit quasi esse accusamus possimus ipsa ipsam, a debitis ullam eum atque iure deleniti totam facilis! Tempore maxime quam mollitia ipsa itaque tenetur debitis, voluptatibus nobis perferendis! Itaque fuga deserunt sapiente.
     </p>
-    <Component2 />
     </div>
-  );
+    )
 }
 
-export default App;
-
-
-//Components - class(class based components) , functions(function based components)
+export default Component2
