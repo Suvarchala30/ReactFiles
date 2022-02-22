24c3ac0dd4af44b42cf034a18f497c883f29b033
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-22 23:00:06 +0530

    secondreact

--- first-app/src/components/Component1/Component1.js
+++ first-app/src/components/Component1/Component1.js
@@ -1,13 +1,17 @@
-import React from "react";//This is optional in function based components
+import React from "react"; //This is optional in function based components
 import Developer from "./Developer/Developer";
-import './Component1.css'
-function Component1(){
-    return (
-        <div className="Component1">
-        <h1 style={ {backgroundColor:'black', color:'white'}}>A Heading</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, alias ea. Exercitationem sint veniam sunt laborum eligendi earum neque vitae maxime. Accusantium earum cum ipsam porro magni rem iste odit.</p>
-        <Developer />
-        </div>
-    )
+import "./Component1.css";
+function Component1() {
+  return (
+    <div className="Component1">
+      <h1 style={{ backgroundColor: "black", color: "white" }}>A Heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, alias ea.
+        Exercitationem sint veniam sunt laborum eligendi earum neque vitae
+        maxime.. Accusantium earum cum ipsam porro magni rem iste odit.
+      </p>
+      <Developer />
+    </div>
+  );
 }
-export default Component1
+export default Component1;
