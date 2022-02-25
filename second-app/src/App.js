f8ccfe83afa7f98d481b1bf087482c1e8359160e
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-25 10:25:41 +0530

    feb 24

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,17 +1,38 @@
-import './App.css';
-import Hello from './Components/Hello';
-import HelloClassBased from './Components/Hello-ClassBased'
+import React from "react";
+import "./App.css";
+//import React, {Component} from 'react';
+//import Hello from './Components/Hello';
+import HelloClassBased from "./Components/Hello-ClassBased";
 
-function App() {
-  return (
-    <div className="App">
-      <HelloClassBased greetTo="World!"/>
-    {/*
-      <Hello greetTo="World!" />
-       <Hello greetTo="America" />
-      <Hello greetTo="Canada" greet="Australia"/> */}
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <HelloClassBased greetTo="World!"/>
+//       <HelloClassBased greetTo="Africa!"/>
+//       <HelloClassBased greetTo="Canada!"/>
+//     {/*
+//       <Hello greetTo="World!" />
+//        <Hello greetTo="America" />
+//       <Hello greetTo="Canada" greet="Australia"/> */}
+//     </div>
+//   );
+// }
+
+//class App extends Component{
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    //console.log(this.props)
+  }
+  render() {
+    return (
+      <div className="App">
+        <HelloClassBased />
+        {/* <HelloClassBased greetTo="Africa!" />
+        <HelloClassBased greetTo="Canada!" /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- second-app/src/Components/Hello-ClassBased.js
+++ second-app/src/Components/Hello-ClassBased.js
@@ -1,11 +1,30 @@
 import React , { Component } from "react";
 
 class HelloClass extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            greetTo:'World',
+            greetAlsoTo:'Hyderabad'
+        }
+       // console.log(this.props)
+      }
+
+      changeGreetTo = () =>{
+          this.setState({
+              greetTo:'India',
+              greetAlsoTo:'Vizag'
+          })
+        console.log('Iam changed greet')
+      }
     render(){
+       // console.log(this.props)
+       const { greetTo,greetAlsoTo } = this.state
     return(
         <div className="HelloClass">
-            <h1>Hello</h1>
+            <h1>Hello { greetTo } and {greetAlsoTo}</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi natus laborum eum porro dolore vero, accusamus molestias deleniti architecto dicta necessitatibus eaque accusantium, blanditiis iste omnis illum repudiandae expedita ipsum. Animi, voluptates? Porro quod dolorum sit rem perspiciatis laudantium ratione illo odio tempore? Pariatur optio error perferendis ea excepturi eius.</p>
+            <button onClick={this.changeGreetTo}>ChangegreetTo</button>
         </div>
     )
     }
