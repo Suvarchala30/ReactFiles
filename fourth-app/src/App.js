6edb17233cf556e36d6ab0fc20ebcd49eff5f42b
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-03-04 23:04:55 +0530

    mar4end

--- fourth-app/src/App.js
+++ fourth-app/src/App.js
@@ -1,11 +1,32 @@
-
+import React,{ useState, useEffect } from 'react';
 import './App.css';
+import TableComponent from "./Components/TableComponent"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Button, ProgressBar} from "react-bootstrap"
+
 
 function App() {
+  const [show,setShow] = useState(true)
+  const [counter,setCounter] = useState(0)
+
+  //Replicating componentDidMount using useEffect hook
+  useEffect(()=>{
+    console.log('Component Mounted')
+  },[])
+
+  //Replicating componentDidUpdate using useEffect hook
+  useEffect(()=>{
+    console.log('Counter/show state updated')
+  },[counter,show])
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container className="App">
+     {show &&  <TableComponent />}
+    <Button variant="dark" onClick={()=>setShow(!show)}>{show ? 'Hide' : 'Show'}</Button>
+    <ProgressBar now={counter} />
+    <Button onClick={()=>setCounter(counter+1)} variant="dark">Increment</Button>
+    </Container>
   );
 }
 
