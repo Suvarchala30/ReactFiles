abcda7d503446729ab14d62bb2409564b56102b0
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-03-14 23:01:31 +0530

    mar 14 class

---
+++ eighth-app/redux.js
@@ -0,0 +1,14 @@
+const redux = require('redux')//import redux like this only
+
+//Action type constant
+const INCREMENT_COUNTER='INCREMENT_COUNTER'
+const DECREMENT_COUNTER='DECREMENT_COUNTER'
+
+//Action
+{
+    type: INCREMENT_COUNTER
+}
+
+{
+    type: DECREMENT_COUNTER
+}
